5f98e04f29aa415fcd63a35039ad1a5da27999c2
Sam Holden <samuel99holden@gmail.com>
2019-09-12 09:58:38 +1000

    Prac 12/9
    Removed master details from sln
    added my screen

---
+++ QUTCal/AddClass.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace QUTCal
+{
+    public partial class AddClass : ContentPage
+    {
+        public AddClass()
+        {
+            InitializeComponent();
+        }
+    }
+}
