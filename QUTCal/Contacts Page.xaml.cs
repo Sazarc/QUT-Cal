bc00fd66167041597cddb45f1fdef189112268c0
PaulJoshuaDM <54649766+PaulJoshuaDM@users.noreply.github.com>
2019-09-12 10:49:33 +1000

    Contacts Page
    For the Contacts Page of the App

---
+++ QUTCal/Contacts Page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QUTCal
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Contacts_Page : ContentPage
+    {
+        public Contacts_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
